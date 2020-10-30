64b3e15f890d6ea0d8b9ea118956d59d25b442bb
jangle <shubhamjangale0000@gmail.com>
2020-10-30 09:35:18 +0530

    Added : Inserting values into table

--- payrollQueries.sql
+++ payrollQueries.sql
@@ -17,3 +17,10 @@ start           DATE NOT NULL,
 PRIMARY KEY      (id)
 );
 describe employee_Payroll;
+
+#UC3
+insert into employee_payroll (name, salary ,start) VALUES
+('BILL', 10000000.00, '2018-01-03'),
+('Terisa',20000000.00, '2019-01-03'),
+('Charlie',30000000.00, '2020-12-13');
+select * from employee_payroll;
