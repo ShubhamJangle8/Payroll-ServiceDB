02f82a5671a08a5320de0875c10fa2d110d85970
jangle <shubhamjangale0000@gmail.com>
2020-10-30 09:28:42 +0530

    Added : Added a table to a database with its attributes

--- payrollQueries.sql
+++ payrollQueries.sql
@@ -6,3 +6,14 @@ create database payroll_service;
 show databases;
 use payroll_service;
 select database();
+
+#UC2
+create table employee_payroll
+(
+id              INT unsigned NOT NULL AUTO_INCREMENT,
+name            VARCHAR(150) NOT NULL,
+salary          Double NOT NULL,
+start           DATE NOT NULL,
+PRIMARY KEY      (id)
+);
+describe employee_Payroll;
